8ed2cd92c23239e6cc1fa4398b2671bc0f63d8a7
Kash Khodabakhshi <kashk94@gmail.com>
2019-10-24 17:25:17 -0700

    added login inside login.js

--- login.js
+++ login.js
@@ -3,23 +3,34 @@ const path = require('path');
 const PORT = process.env.PORT || 5000
 var app = express();
 
-// const { Pool } = require('pg');
-// const pool = new Pool({
-//   connectionString: "postgres://postgres:shimarov6929@localhost/assignment2"
-// });
-//
-// const { Pool } = require('pg');
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: true
-// });
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {res.render('pages/index')});
+app.get('/', (req, res) => {
+    var userID=req.body.uname;
+    var loginQuery = `SELECT * FROM logindb WHERE username='${userID}'`;
+    pool.query(loginQuery, (error, result) => {
+        if (error)
+            res.end(error);
+        
+        var userIDs={'rows': result.rows};
+
+        if (!Array.isArray(tokimons['rows']) || !tokimons['rows'].length)
+            res.end("User is not in db!");
+
+        else
+            res.render('pages/home');        
+    })
+
+});
 
 app.post('/home', (req,res) => {
     res.render('pages/home');
