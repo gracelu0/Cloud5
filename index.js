7afa7da610e6207cdb80e68b8b57240a0e35c942
Praneer Shrestha <praneer.shrest@gmail.com>
2019-10-25 17:12:57 -0700

    bcrypt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000
 var app = express();
+const bcrypt = require('bcrypt');
 
 const { Pool } = require('pg');
 var pool = new Pool({
@@ -47,7 +48,7 @@ app.post('/login', (req, res) => {
     var userID = req.body.username;
     var userpwd = req.body.pwd;
     var loginQuery = `SELECT * FROM logindb WHERE username='${userID}'`;
-    pool.query(loginQuery, (error, result) => {
+    pool.query(loginQuery, async (error, result) => {
 
         if (error)
             res.end(error);
@@ -56,7 +57,7 @@ app.post('/login', (req, res) => {
             console.log("not a regular user");
 
         else
-            if(result.rows[0].password == userpwd) {
+            if(await bcrypt.compare(userpwd, result.rows[0].password)) {
                 console.log("Successful login");
                 res.render('pages/home');
             }
@@ -67,19 +68,24 @@ app.post('/login', (req, res) => {
 });
 
 
-app.post('/signUpForm', (req,res) => {
+app.post('/signUpForm', async (req,res) => {
+
     var insertUsername = req.body.username;
     var insertPassword = req.body.password;
     var confirm = req.body.confirmPassword;
     var insertEmail = req.body.email;
-    console.log(req.body);
 
     if(insertPassword !== confirm){
         res.send("Passwords do not match");
         return res.render('pages/signUp');
     }
 
-    var insertquery = `INSERT INTO logindb(username, password, email) VALUES ('${insertUsername}', '${insertPassword}', '${insertEmail}');`
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    console.log(salt);
+    console.log(hashedPassword);
+
+    var insertquery = `INSERT INTO logindb(username, password, email) VALUES ('${insertUsername}', '${hashedPassword}', '${insertEmail}');`
     pool.query(insertquery, (error, result) => {
         if(error){
             res.send("Username is taken!");
