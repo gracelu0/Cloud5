84553174c5530c51cc1c7e28045a3ec9c9d84dd6
Kash Khodabakhshi <kashk94@gmail.com>
2019-10-27 23:41:56 -0700

    Added on-screen messages to current pages

--- game.js
+++ game.js
@@ -1,13 +1,9 @@
 const express = require('express');
-const session = require('express-session');
-const flash = require('express-flash');
 const path = require('path');
 const PORT = process.env.PORT || 5000
 
 var app = express();
 const bcrypt = require('bcrypt');
-
-var app = express();
 
 const { Pool } = require('pg');
 var pool = new Pool({
@@ -21,17 +17,9 @@ app.use(express.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(session({
-	saveUninitialized: false,
-	resave: 'false',
-	secret: 'This is for our flash messages'
-}))
-app.use(flash());
-
 app.get('/', (req, res) => {
-    res.render('pages/login')
+    res.render('pages/login');
 });
-
 
 app.post('/home', (req,res) => {
     res.render('pages/home');
@@ -67,23 +55,18 @@ app.post('/login', (req, res) => {
         if (error)
             res.end(error);
 
-        if(result.rows.length === 0)
-            res.render("pages/errors/wrongUser");
+        if (result.rows.length === 0)
+            res.render('pages/login', {loginMessage: 'Username entered does not match any accounts! Please sign up for a new account below.'});
 
         else{
-            if(await bcrypt.compare(userpwd, result.rows[0].password)) {
-                console.log("Login successful");
-                if (result.rows[0].usertype == 'User'){
-                    res.render('pages/home');
+            if(await bcrypt.compare(userpwd, result.rows[0].password)){
+                if (result.rows[0].usertype == 'User')
+                    res.render('pages/home', {message: 'Successfully logged in!'});
                 else // result.row[0].usertype == 'Admin'
-                    res.render('pages/admin');
-
-
+                    res.render('pages/admin', {message: 'Successfully logged in as an admin!'});
             }
-            else {
-                res.render("pages/errors/wrongPwd");
-            }
-          
+            else 
+                res.render('pages/login', {loginMessage: 'Password entered is incorrect! Please try again.'});
         }
     });
 });
@@ -97,21 +80,18 @@ app.post('/signUpForm', async (req,res) => {
     var insertEmail = req.body.email;
 
     if(insertPassword !== confirm){
-        res.render("pages/errors/pwdSignUp");
+        res.render('pages/signUp', {message: 'Passwords do not match!'});
     }
     else{
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    console.log(salt);
-    console.log(hashedPassword);
 
     var insertquery = `INSERT INTO logindb(username, password, email) VALUES ('${insertUsername}', '${hashedPassword}', '${insertEmail}');`
     pool.query(insertquery, (error, result) => {
         if(error){
-            res.send("Username is taken!");
-            return res.render('pages/signUp');
+            return res.render('pages/signUp', {message: 'Username already taken!'});
         }
-        return res.render('pages/login');
+        return res.render('pages/login', {signupMessage: 'New user created!'});
     });
   }
 });
