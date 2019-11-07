5363fefaa89d9ea470f0f7def8bc6eef48f6564d
Grace Luo <grace.r.luo@gmail.com>
2019-11-06 19:15:54 -0800

    map

--- public/js/gameScript.js
+++ public/js/gameScript.js
@@ -4,7 +4,11 @@ var config = {
     height: 600,
     parent: 'phaser',
     physics: {
-        default: 'arcade'
+        default: 'arcade',
+        arcade: {
+            gravity: {y: 0},
+            debug: true
+        }
     },
 
     scene: {
@@ -21,8 +25,10 @@ const game = new Phaser.Game(config);
 function preload(){
     
     this.load.image('RPGpack', 'assets/RPGpack_sheet.png');
+    this.load.image('overworld', 'assets/overworld.png');
     this.load.tilemapTiledJSON('map', 'assets/bbmap.json');
 
+    
     this.load.image('testingmap', 'assets/testSheet.png' );
     this.load.image('test2', 'assets/tileSheet1.png');
     this.load.image('player','assets/alienPink.png');
@@ -32,20 +38,36 @@ function preload(){
 function create(){
     map = this.add.tilemap('map');
     var groundTiles = map.addTilesetImage('RPGpack');
-
+    var bridgeTiles = map.addTilesetImage('overworld');
     groundLayer = map.createStaticLayer('Below Player', groundTiles, 0, 0);
+    bridgeLayer = map.createStaticLayer('Below Player 2', bridgeTiles,0,0);
     collideLayer = map.createStaticLayer('World', groundTiles, 0, 0);
 
-    player = this.add.sprite(200,400,'player');
-    
+    //set boundaries of game world
+    this.physics.world.bounds.width = groundLayer.width;
+    this.physics.world.bounds.height = groundLayer.height;
+
+
+    player = this.physics.add.sprite(200,400,'player');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
+    collideLayer.setCollisionBetween(200,240);
+
+    //collideLayer.setCollisionByProperty({collides:true});
+    this.physics.add.collider(collideLayer,player);
+
+    const debugGraphics = this.add.graphics().setAlpha(0.75);
+    collideLayer.renderDebug(debugGraphics, {
+        tileColor: null, // Color of non-colliding tiles
+        collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+        faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+    });
+
     cursors = this.input.keyboard.createCursorKeys();
     //set bounds for camera (game world)
     this.cameras.main.setBounds(0,0,map.widthInPixels, map.heightInPixels);
     //make camera follow player
     this.cameras.main.startFollow(player);
-
-
-    
 
     
 
