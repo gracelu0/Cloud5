6c9e269406d360d7b2803b806047ca7fa062fe27
unknown <stevenzhu0811@gmail.com>
2019-11-15 02:24:15 -0800

    collision of all objects working, bullets traps and health bars added, debugging and formatting

--- game.js
+++ game.js
@@ -13,8 +13,8 @@ const bcrypt = require('bcrypt');
 
 const { Pool } = require('pg');
 var pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-  //connectionString: "postgres://postgres:shimarov6929@localhost/cloud5"
+  //connectionString: process.env.DATABASE_URL
+  connectionString: 'postgres://postgres:1234@localhost/login'
 });
 
 
