d34b4143b0ef2ae6c3dda4075864a8da89fc28df
unknown <stevenzhu0811@gmail.com>
2019-11-17 03:29:58 -0800

    multiplayer bullets, bullet collisions and deaths emits working

--- public/js/gameScript.js
+++ public/js/gameScript.js
@@ -32,7 +32,7 @@ var shootButton;
 var lastFired = 0;
 var lastBomb = 0;
 var facing = 1;
-var ammunition = 10;
+var ammunition = 100;
 var x;
 var y;
 
@@ -224,18 +224,18 @@ class Bullet extends Phaser.Physics.Arcade.Sprite{
     scene.physics.world.enable(this);
     if(facing == 1){
       this.xSpeed = Phaser.Math.GetSpeed(0,1);
-      this.ySpeed = Phaser.Math.GetSpeed(-400,1);
+      this.ySpeed = Phaser.Math.GetSpeed(-4000,1);
     } 
     else if(facing == 2){
       this.xSpeed = Phaser.Math.GetSpeed(0,1);
-      this.ySpeed = Phaser.Math.GetSpeed(400,1);
+      this.ySpeed = Phaser.Math.GetSpeed(4000,1);
     }
     else if(facing == 3){
-      this.xSpeed = Phaser.Math.GetSpeed(-400,1);
+      this.xSpeed = Phaser.Math.GetSpeed(-4000,1);
       this.ySpeed = Phaser.Math.GetSpeed(0,1);
     }
     else if(facing == 4){
-      this.xSpeed = Phaser.Math.GetSpeed(400,1);
+      this.xSpeed = Phaser.Math.GetSpeed(4000,1);
       this.ySpeed = Phaser.Math.GetSpeed(0,1);
     }
   }
@@ -248,12 +248,13 @@ class Bullet extends Phaser.Physics.Arcade.Sprite{
   }
   update(time, delta){
     if(this){
-      this.x += this.xSpeed * delta;
-      this.y += this.ySpeed * delta;
+      this.x += this.xSpeed;
+      this.y += this.ySpeed;
       this.setPosition(this.x, this.y);
     }
 
-    if(this.x > this.bulletInitX + 500 || this.x < this.bulletInitX - 500 || this.y > this.bulletInitY + 500 || this.y < this.bulletInitY - 500){
+    if(this.x > this.bulletInitX + 500 || this.x < this.bulletInitX - 500 || this.x < -10 || this.x > groundLayer.width ||
+      this.y > this.bulletInitY + 500 || this.y < this.bulletInitY - 500 || this.y < -10 || this.y > groundLayer.width){
       this.destroy();
     }
   }
@@ -338,12 +339,17 @@ function create(){
   map.setCollisionBetween(1,999,true,collideLayer);
 
   var self = this;
+  var sessionId;
   this.socket = io();
   this.otherPlayers = this.physics.add.group();
 
   //character selection
   var selected = document.getElementById('colour').innerHTML;
   console.log(selected);
+
+  this.socket.on('connect', () => {
+    sessionId = this.socket.id;
+  });
 
   this.socket.on('currentPlayers', function (players) {
     Object.keys(players).forEach(function (id) {
@@ -375,6 +381,36 @@ function create(){
         }
       }.bind(this));
     }.bind(this));
+
+    this.socket.on('player-hit', function(id){
+      if(id === sessionId){
+        self.player.health -= 10;
+      }
+      else{
+        self.otherPlayers.getChildren().forEach(function (otherPlayer){
+          if(id === otherPlayer.playerId){
+            otherPlayer.health -= 10;
+          }
+        })
+      }
+    });
+  
+    this.socket.on('bulletsUpdate', function(servBullets){
+      var counter = 0;
+      bullets.getChildren().forEach(child => {
+        if(servBullets[counter]){
+          child.x = servBullets[counter].x;
+          child.y = servBullets[counter].y;  
+        }
+        counter++;
+        if(counter > servBullets.length){
+          child.destroy();
+        }
+      })
+      for(var i = counter; i < servBullets.length; i++){
+        addBullets(self,servBullets[i]);
+      }
+    });
 
   this.socket.on('playerMoved', function (playerInfo) {
     self.otherPlayers.getChildren().forEach(function (otherPlayer) {
@@ -408,89 +444,104 @@ function create(){
   this.bombButton = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
 
   camera = this.cameras.main;
-  ammoCount = this.add.text(0, 0,"Ammunition Count:" + ammunition + "/10");
+  ammoCount = this.add.text(0, 0,"Ammunition Count:" + ammunition + "/100");
   //set bounds for camera (game world)
   camera.setBounds(0,0,map.widthInPixels, map.heightInPixels);
 }
 
 function update(){
   if(this.player){
-    if (this.cursors.up.isDown){
-      this.player.body.position.y -=4;
-      this.player.flipY = true;
-      facing = 1;
-    }
-    if (this.cursors.down.isDown){
-      this.player.body.position.y +=4;
-      this.player.flipY = false;
-      facing = 2;
-    }
-    if (this.cursors.left.isDown){
-      this.player.body.position.x -=4;
-      this.player.flipX = true;
-      facing = 3;
-    }
-    if (this.cursors.right.isDown){
-      this.player.body.position.x +=4;
-      this.player.flipX = false;
-      facing = 4;
-    }
+    if(this.player.health > 0){
+      if (this.cursors.up.isDown){
+        this.player.body.position.y -=4;
+        this.player.flipY = true;
+        facing = 1;
+      }
+      if (this.cursors.down.isDown){
+        this.player.body.position.y +=4;
+        this.player.flipY = false;
+        facing = 2;
+      }
+      if (this.cursors.left.isDown){
+        this.player.body.position.x -=4;
+        this.player.flipX = true;
+        facing = 3;
+      }
+      if (this.cursors.right.isDown){
+        this.player.body.position.x +=4;
+        this.player.flipX = false;
+        facing = 4;
+      }
 
-    if (this.player.health > 0) {
-      this.healthbar_green.displayWidth = (this.player.health/100)*100;
-      this.healthbar_green.x = this.player.body.position.x + 12;
-      this.healthbar_green.y = this.player.body.position.y - 20;
-      this.healthbar_red.x = this.player.body.position.x + 12;
-      this.healthbar_red.y = this.player.body.position.y - 20;
-    }
+      if(this.player.body.position.x - 455 > 0 && this.player.body.position.x + 495 < groundLayer.width){
+        ammoCount.x = this.player.body.position.x - 455;
+      }
+      if(this.player.body.position.y - 285 > 0 && this.player.body.position.y + 335 < groundLayer.height){
+        ammoCount.y = this.player.body.position.y - 285;
+      }
 
-    if (this.cursors.space.isDown && ammunition > 0 && lastFired == 0){
-      var bullet = bullets.get();
-    
-      if(bullet){
-        if(soundFlag == true) {
-          shootSound.play();
+      if (this.player.health > 0) {
+        this.healthbar_green.displayWidth = (this.player.health/100)*100;
+        this.healthbar_green.x = this.player.body.position.x + 12;
+        this.healthbar_green.y = this.player.body.position.y - 20;
+        this.healthbar_red.x = this.player.body.position.x + 12;
+        this.healthbar_red.y = this.player.body.position.y - 20;
+      }
+
+      if (this.cursors.space.isDown && ammunition > 0 && lastFired == 0){
+        var bullet = bullets.get();
+      
+        if(bullet){
+          if(soundFlag == true) {
+            shootSound.play();
+          }
+          bullet.fire(this.player.body.position.x, this.player.body.position.y);
+          lastFired = 10;
+          ammunition --;
+          ammoCount.setText("Ammunition Count:" + ammunition +"/100");
+          this.socket.emit('bulletFire', { x: this.player.body.position.x, y: this.player.body.position.y, xSpeed:bullet.xSpeed, ySpeed:bullet.ySpeed});
+          }
+      }
+      if(lastFired > 0){
+        lastFired --;
+      }
+
+      if (this.bombButton.isDown && lastBomb == 0){
+        var trap = traps.create(this.player.body.position.x, this.player.body.position.y, 'bomb');
+        trap.body.setImmovable();
+        lastBomb = 30;
+      }
+      if(lastBomb > 0){
+        lastBomb --;
+      }
+
+      this.physics.collide(this.player,collideLayer);
+      this.physics.collide(this.player,this.otherPlayers);
+      
+      this.otherPlayers.getChildren().forEach(child => {
+        child.body.immovable = true;
+        if(child.health <= 0){
+          this.socket.emit('playerDied', {id:child.playerId});
+          playerDeath(child);
         }
-        bullet.fire(this.player.body.position.x, this.player.body.position.y);
-        lastFired = 10;
-        ammunition --;
-        ammoCount.setText("Ammunition Count:" + ammunition +"/10");
+      })
+
+      x = this.player.x;
+      y = this.player.y;
+      if (this.player.oldPosition && (x !== this.player.oldPosition.x || y !== this.player.oldPosition.y)) {
+        this.socket.emit('playerMovement', { x: this.player.x, y: this.player.y});
+      }
+
+      this.player.oldPosition = {
+        x: this.player.x,
+        y: this.player.y,
       }
     }
-    if(lastFired > 0){
-      lastFired --;
-    }
-
-    if (this.bombButton.isDown && lastBomb == 0){
-      var trap = traps.create(this.player.body.position.x, this.player.body.position.y, 'bomb');
-      trap.body.setImmovable();
-      lastBomb = 30;
-    }
-    if(lastBomb > 0){
-      lastBomb --;
-    }
-
-    this.physics.collide(this.player,collideLayer);
-    this.physics.collide(this.player,this.otherPlayers);
-    bullets.getChildren().forEach(child => {
-      this.physics.collide(child, this.otherPlayers, bulletCollision);
-    })
-    this.otherPlayers.getChildren().forEach(child => {
-      child.body.immovable = true;
-      if(child.health <= 0){
-        playerDeath(child);
-      }
-    })
-
-    x = this.player.x;
-    y = this.player.y;
-    if (this.player.oldPosition && (x !== this.player.oldPosition.x || y !== this.player.oldPosition.y)) {
-      this.socket.emit('playerMovement', { x: this.player.x, y: this.player.y});
-    }
-
-    this.player.oldPosition = {
-      x: this.player.x,
-      y: this.player.y,
+    else{
+      this.socket.emit('playerDied', {id: this.player.playerId});
+      playerDeath(this.player);
+      this.healthbar_green.destroy();
+      this.healthbar_red.destroy();
     }
   }
 
@@ -590,15 +641,19 @@ function addOtherPlayers(self, playerInfo) {
   self.otherPlayers.add(otherPlayer);
 }   
 
-
+function addBullets(self, bulletInfo){
+  const nBullet = self.add.sprite(bulletInfo.x, bulletInfo.y, 'bulletImg');
+  bullets.add(nBullet);
+}
 
 bulletCollision = function(bullets,hitPlayer){
-  bullets.destroy();
-  hitPlayer.health -= 10;
+    bullets.destroy();
+    hitPlayer.destroy();
 }
 
 playerDeath = function(deadPlayer){
   deadPlayer.destroy();
+  deadPlayer = null;
   //healthbar_red.destroy();
   //healthbar_green.destroy();
 }
