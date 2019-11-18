cce452fbafaec7bc501af2bf2185dec7e70b06e0
Grace Luo <grace.r.luo@gmail.com>
2019-11-17 21:10:32 -0800

    ui for game screen and how to play screen

--- game.js
+++ game.js
@@ -15,12 +15,6 @@ var pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
 
-// var pool = new Pool({
-//   user: 'graceluo',
-//   password: 'tokicorgi',
-//   host: 'localhost',
-//   database: 'cloud5'
-// });
 
 
 
