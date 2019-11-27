8c85a01f7d88f8b16f350c0c214670a7c2e34204
Grace Luo <grace.r.luo@gmail.com>
2019-11-26 17:39:11 -0800

    merged

--- test/test_game.js
+++ test/test_game.js
@@ -3,39 +3,13 @@ var chai = require('chai')
     , assert = chai.assert
     , should = chai.should();
 
-var game = require('../game').app;
-var request = require('supertest');
-
-//set up data to pass to login method
-const userCredentials = {
-    username: 'mojo123',
-    password: '12345'
-}
-
-//login user
-var authenticatedUser = request.agent(game);
-
-describe('login', ()=>{
-    it("user can log in", (done)=>{
-        authenticatedUser
-            .post('/login')
-            .send(userCredentials)
-            .end((err,res)=>{
-                expect(res.statusCode).to.equal(200);
-                expect('Location', '/admin');
-                done();
-            });
-    });
-});
-
-
-var req = require('request'),
+    var request = require('request'),
     base_url = "http://localhost:5000";
 
 describe('server access', () =>{
     describe("GET /", ()=>{
         it("returns status code 200", (done)=>{
-            req.get(base_url,(err,res,body)=>{
+            request.get(base_url,(err,res,body)=>{
                 assert.equal(200, res.statusCode);
                 done();
             });
